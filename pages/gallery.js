6b84408db2739a8a4a73974b232a3dc8e2db6394
XeDiS <admin@voidxedis.com>
2024-10-31 17:21:04 -0700

    hmm

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Layout from '../components/Layout';
 import Cookies from 'js-cookie';
-import DeviationTable from '../components/DeviationTable';
 
 const Gallery = () => {
   const [data, setData] = useState([]);
@@ -24,14 +22,69 @@ const Gallery = () => {
   }, []);
 
   return (
-    <Layout>
+    <div>
       <h1>Gallery</h1>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <DeviationTable data={data} />
+        <table>
+          <thead>
+            <tr>
+              <th>Deviation ID</th>
+              <th>Meta Dev ID</th>
+              <th>Title</th>
+              <th>Link</th>
+              <th>Published</th>
+              <th>Mature</th>
+              <th>Stats</th>
+              <th>Tier Deviation ID</th>
+              <th>Downloadable</th>
+              <th>Tier Name</th>
+              <th>Tier URL</th>
+              <th>Gallery Name</th>
+              <th>Premium Gallery ID</th>
+              <th>Prem Type</th>
+              <th>Dollar Price</th>
+              <th>Num Subs</th>
+              <th>Num Views</th>
+              <th>Thumbs Link</th>
+              <th>Mature Level</th>
+              <th>Mature Class</th>
+              <th>Tags</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((deviation, index) => (
+              <tr key={index}>
+                <td>{deviation.deviationID}</td>
+                <td>{deviation.metaDevID}</td>
+                <td>{deviation.title}</td>
+                <td>{deviation.link}</td>
+                <td>{deviation.published}</td>
+                <td>{deviation.mature}</td>
+                <td>{deviation.stats}</td>
+                <td>{deviation.tierDeviationID}</td>
+                <td>{deviation.downloadable}</td>
+                <td>{deviation.tierName}</td>
+                <td>{deviation.tierURL}</td>
+                <td>{deviation.galleryName}</td>
+                <td>{deviation.premiumGalleryID}</td>
+                <td>{deviation.premType}</td>
+                <td>{deviation.dollarPrice}</td>
+                <td>{deviation.numSubs}</td>
+                <td>{deviation.numViews}</td>
+                <td>{deviation.thumbsLink}</td>
+                <td>{deviation.matureLevel}</td>
+                <td>{deviation.matureClass}</td>
+                <td>{deviation.tags}</td>
+                <td>{deviation.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
-    </Layout>
+    </div>
   );
 };
 
