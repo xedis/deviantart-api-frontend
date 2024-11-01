c55b988aba3923f70a02c499e9f3a2b7a0734743
XeDiS <admin@voidxedis.com>
2024-10-31 17:13:29 -0700

    hmm

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,5 +1,15 @@
 import { redirectToAuth, exchangeCodeForToken } from '../../utils/auth';
 
+/**
+ * Handles authorization code flow for DeviantArt API.
+ *
+ * If no code is present, redirects to DeviantArt authorization page.
+ * If a code is present, exchanges it for an access token and redirects to /gallery.
+ *
+ * @param {Object} req Express request object
+ * @param {Object} res Express response object
+ * @return {Promise<void>} Resolves when the request has been handled.
+ */
 export default async function handler(req, res) {
   console.log('Auth handler called with query:', req.query);
   console.log('Auth handler cookies:', req.cookies);
@@ -8,10 +18,13 @@ export default async function handler(req, res) {
   if (!code) {
     console.log('No code present, redirecting to auth');
     redirectToAuth(res);
+  } else if (!state) {
+    console.error("No state present in query, can't exchange code for token");
+    res.status(400).json({ error: 'No state present in query, can\'t exchange code for token' });
   } else {
     try {
       console.log('Exchanging code for token');
-      const { access_token, refresh_token } = await exchangeCodeForToken(code, state, req, res);
+      await exchangeCodeForToken(code, state, req, res);
       console.log('Token exchange successful, redirecting to home');
       res.redirect('/gallery');
     } catch (error) {
